ac558e6b28254e5708748bf2272a9b5c77b4da2e
yuki <1286060599@qq.com>
2022-03-22 23:27:57 +0800

    Java方法第一次提交

---
+++ com/yuki/scanner/Demo03.java
@@ -0,0 +1,16 @@
+package com.yuki.scanner;
+
+import java.util.Scanner;
+/**
+ * 普通方式
+ */
+public class Demo03 {
+    public static void main(String[] args) {
+        //从键盘接受数据
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入数据：");
+        String str = scanner.nextLine();
+        System.out.println("输出内容为："+str);
+        scanner.close();
+    }
+}
